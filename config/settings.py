0c079a3848274c6e9e9d222ce6410022944ff970
lendrailham-7 <lendra.ramadhan0710@gmail.com>
2026-06-16 12:26:28 +0700

    balik

--- config/settings.py
+++ config/settings.py
@@ -138,18 +138,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
